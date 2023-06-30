d6574927504c525508014ef04f03b0612b0a5337
Ritika11294 <ritikagupta11294@gmail.com>
2023-06-30 20:05:52 +0530

    link updated

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => { 
   return mongoose.connect(
-    "mongodb+srv://ritika:nIcna3AVlglBs1fO@cluster0.lcegw.mongodb.net/e-commerce?retryWrites=true&w=majority"
+    "mongodb+srv://ritika:nIcna3AVlglBs1fO@cluster0.lcegw.mongodb.net/e-commerce"
   )
   
 };
